45a3fba6b067765d5a9488b71ef8b12dd4d219bb
Rob Ladbrook <robl@theappbusiness.com>
2015-05-18 12:52:46 +0100

    Initial setup of plugin

---
+++ Tab.Slack.Bot.Plugins/ZenDesk/ZenDeskHandler.cs
@@ -0,0 +1,47 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tab.Slack.Bot.Integration;
+using Tab.Slack.Common.Model.Events;
+using ZendeskApi_v2;
+using ZendeskApi_v2.Models.Tickets;
+
+namespace Tab.Slack.Bot.Plugins.ZenDesk
+{
+    public class ZenDeskHandler : MessageHandlerBase, IMessageHandler
+    {
+        private IZendeskApi zenDeskApi;
+
+        public bool CanHandle(EventMessageBase message)
+        {
+            return message.MatchesText(@"(can|could|would) (I|you) please.+\?");
+        }
+
+        public async Task<ProcessingChainResult> HandleMessageAsync(EventMessageBase message)
+        {
+            // TODO: figure out config approach
+            this.zenDeskApi = new ZendeskApi("url", "username", apiToken: "token");
+
+            var ticket = new Ticket
+            {
+                Requester = new Requester
+                {
+                    Name = "name of slack user",
+                    Email = "email of slack user"
+                },
+                Subject = "message text",
+                Comment = new Comment
+                {
+                    Body = "message text"
+                }
+            };
+
+            await this.zenDeskApi.Tickets.CreateTicketAsync(ticket);
+
+            return ProcessingChainResult.Continue;
+        }
+    }
+}
